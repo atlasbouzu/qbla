347ced96cb1622fed0a52580d04112129bb14ab7
ktktbouzu <athomaslab@gmail.com>
2025-04-08 23:33:16 +0800

    Add query execute function

--- scripts/utils/db.py
+++ scripts/utils/db.py
@@ -4,13 +4,19 @@ class Database:
     def __init__(self, creds):
         self.conn = self.create_conn(creds)
         self.cursor = self.init_conn()
-
+    
     def create_conn(self, creds):
         return psycopg2.connect(**creds)
-
+    
     def init_conn(self):
         return self.conn.cursor()
-
+    
     def test(self, message):
         print(message)
+    
+    def execute(self, query):
+        try:
+            return self.cursor.execute(query)
+        except psycopg2.errors.UndefinedTable:
+            return False
 
