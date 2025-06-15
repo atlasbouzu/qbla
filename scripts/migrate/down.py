e90de4db5323e53ebb8d3b1b1f80cba18b9f4428
atlasbouzu <at.labarda@tuta.com>
2025-06-15 20:11:56 +0800

    Pull migration history from DB

--- scripts/migrate/down.py
+++ scripts/migrate/down.py
@@ -7,3 +7,19 @@ def execute(db_conn, opts={}, args=[]):
     print("[PROCESSING] Preparing to execute migration files...")
     
     print("Executing down queries from migrations...")
+    migration_history = get_migration_history(db_conn)
+    print(migration_history)
+
+
+def get_migration_history(db_conn):
+    past_migrations = []
+    
+    try:
+        with db_conn.cursor() as cur:
+            cur.execute("SELECT * FROM schema_migrations ORDER BY name DESC")
+            past_migrations = list(map(lambda record: record[0], cur.fetchall()))
+            db_conn.commit()
+    except:
+        db_conn.rollback()
+
+    return past_migrations
