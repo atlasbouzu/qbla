94095651eb64d734b8f206100d6a425e362ee668
atlasbouzu <at.labarda@tuta.com>
2025-06-21 12:09:37 +0800

    Transfer create migration file to utils

--- scripts/migrate/create.py
+++ scripts/migrate/create.py
@@ -3,7 +3,7 @@ import yaml
 
 from datetime import datetime
 
-from . import constants
+from . import utils
 
 def execute(args, opts):
     if not args_is_valid(args):
@@ -16,17 +16,8 @@ def execute(args, opts):
     
     print("Creating {}...".format(full_filename))
     
-    file_path = os.path.join(constants.MIGRATIONS_PATH,"{}.yaml".format(full_filename))
-    
-    os.umask(0)
-    
-    with open(file_path, 'w', opener=file_opener) as fs:
-        yaml.dump({'down': "", "up": ""}, fs, default_flow_style=False)
-    
+    utils.create_migration_file(full_filename)
     print("[SUCCESS] {} created!".format(full_filename))
-
-def file_opener(file_path, flags):
-    return os.open(file_path, flags, 0o777)
 
 def args_is_valid(args):
     if len(args) == 0:
--- scripts/migrate/utils.py
+++ scripts/migrate/utils.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, yaml
 
 from . import constants
 
@@ -9,4 +9,15 @@ def read_migration_file(filename):
     file.close()
     
     return queries
+
+def create_migration_file(filename):
+    file_path = os.path.join(constants.MIGRATIONS_PATH,"{}.yaml".format(filename))
+    
+    os.umask(0)
+    
+    with open(file_path, 'w', opener=file_opener) as fs:
+        yaml.dump({'down': "", "up": ""}, fs, default_flow_style=False)
+
+def file_opener(file_path, flags):
+    return os.open(file_path, flags, 0o777)
 
